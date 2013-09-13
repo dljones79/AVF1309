c0d3dea0a0e36eac98abf71d3922374b8aab91a1
David Jones <dljones79@fullsail.edu>
2013-09-12 21:38:45 -0500

    Work on css and instagram
    Custom CSS and Instagram functionality work.

--- avfDemo/platforms/ios/www/js/main.js
+++ avfDemo/platforms/ios/www/js/main.js
@@ -2,5 +2,24 @@
 // AVF 1309
 // Demo App
 
+document.addEventListener("deviceready", onDeviceReady, false);
 
+function onDeviceReady() {
+		$("#insta").on("pageinit", displayInstaPics);
+}; // phonegap deviceready
 
+var displayInstaPics = function(){
+	var instaUrl = "https://api.instagram.com/v1/tags/scenery/media/recent?callback=?&amp;client_id=
+		5972228563da4106aa1323119426f70a";
+	
+	$.getJSON(instaUrl, function(instagramData){
+		console.log(instagramData);
+		
+		$.each(instagramData.data, function(index, picture){
+			var pics = "<li><img src='" + picture.images.standard_resolution.url + "' alt='" +
+				picture.user.id + "' /><h4>" + picture.user.ful_name + ", <em>(" + picture.user.username +
+				")</em></h4></li>";
+			$("#dataOut").append(pics);
+		});
+	});
+};
