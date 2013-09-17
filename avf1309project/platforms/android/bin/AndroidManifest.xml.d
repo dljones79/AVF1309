1ee2eb46a5c487719ec121fb7ecf18ad8ca84f6d
David Jones <dljones79@fullsail.edu>
2013-09-17 18:53:33 -0500

    Deleted un-needed files.
    Deleted newly made project.  Working with old project files again

--- avf1309project/platforms/android/bin/AndroidManifest.xml.d
+++
@@ -1,2 +0,0 @@
-/Users/David/Desktop/AVF1309/avf1309project/platforms/android/bin/AndroidManifest.xml : \
-/Users/David/Desktop/AVF1309/avf1309project/platforms/android/AndroidManifest.xml \
--- avf1309project/platforms/android/bin/avfDemoApp-debug-unaligned.apk.d
+++
@@ -1,4 +0,0 @@
-/Users/David/Desktop/AVF1309/avf1309project/platforms/android/bin/avfDemoApp-debug-unaligned.apk : \
-/Users/David/Desktop/AVF1309/avf1309project/platforms/android/bin/avfDemoApp.ap_ \
-/Users/David/Desktop/AVF1309/avf1309project/platforms/android/bin/classes.dex \
-/Users/David/Desktop/AVF1309/avf1309project/platforms/android/libs/cordova-3.0.0.jar \
