aa1f60af2926dc9aafd9bc80b3b099f94a5fedf3
David Jones <dljones79@fullsail.edu>
2013-09-18 19:42:50 -0500

    Testing plugins
    Testing plugins with sample Phonegap code.  Will be replacing shorly
    once verified.

--- avfDemo/platforms/android/assets/www/cordova_plugins.js
+++ avfDemo/platforms/android/assets/www/cordova_plugins.js
@@ -195,6 +195,34 @@ module.exports = [
         "clobbers": [
             "navigator.geolocation"
         ]
+    },
+    {
+        "file": "plugins/org.apache.cordova.core.camera/www/CameraConstants.js",
+        "id": "org.apache.cordova.core.camera.Camera",
+        "clobbers": [
+            "Camera"
+        ]
+    },
+    {
+        "file": "plugins/org.apache.cordova.core.camera/www/CameraPopoverOptions.js",
+        "id": "org.apache.cordova.core.camera.CameraPopoverOptions",
+        "clobbers": [
+            "CameraPopoverOptions"
+        ]
+    },
+    {
+        "file": "plugins/org.apache.cordova.core.camera/www/Camera.js",
+        "id": "org.apache.cordova.core.camera.camera",
+        "clobbers": [
+            "navigator.camera"
+        ]
+    },
+    {
+        "file": "plugins/org.apache.cordova.core.camera/www/CameraPopoverHandle.js",
+        "id": "org.apache.cordova.core.camera.CameraPopoverHandle",
+        "clobbers": [
+            "CameraPopoverHandle"
+        ]
     }
 ]
 });
--- avfDemo/platforms/ios/www/js/main.js
+++ avfDemo/platforms/ios/www/js/main.js
@@ -2,12 +2,18 @@
 // AVF 1309
 // Demo App
 
+var pictureSource;   // picture source
+    var destinationType; // sets the format of returned value
+
 document.addEventListener("deviceready", onDeviceReady, false);
 
 function onDeviceReady() {
 		$("#insta").on("pageinit", displayInstaPics);
 		$("#weather").on("pageinit", displayWeather);
-		$("#getHeading").on("click", displayHeading);
+		//$("#getHeading").on("click", displayHeading);
+		pictureSource=navigator.camera.PictureSourceType;
+        destinationType=navigator.camera.DestinationType;
+        navigator.geolocation.getCurrentPosition(onSuccess, onError);
 }; // phonegap deviceready
 
 // Function to get pictures from Instagram API
@@ -47,6 +53,7 @@ var displayWeather = function(){
 	});	
 };
 
+/*
 var displayHeading = function(){
 	navigator.compass.getCurrentHeading(headingSuccess, headingError);
 	
@@ -58,4 +65,99 @@ var displayHeading = function(){
 		alert('Compass has an error: ' + compassError.code);
 	}
 }
+*/
 
+/////////////////////////////////
+
+    // Called when a photo is successfully retrieved
+    //
+    function onPhotoDataSuccess(imageData) {
+      // Uncomment to view the base64-encoded image data
+      // console.log(imageData);
+
+      // Get image handle
+      //
+      var smallImage = document.getElementById('smallImage');
+
+      // Unhide image elements
+      //
+      smallImage.style.display = 'block';
+
+      // Show the captured photo
+      // The inline CSS rules are used to resize the image
+      //
+      smallImage.src = "data:image/jpeg;base64," + imageData;
+    }
+
+    // Called when a photo is successfully retrieved
+    //
+    function onPhotoURISuccess(imageURI) {
+      // Uncomment to view the image file URI
+      // console.log(imageURI);
+
+      // Get image handle
+      //
+      var largeImage = document.getElementById('largeImage');
+
+      // Unhide image elements
+      //
+      largeImage.style.display = 'block';
+
+      // Show the captured photo
+      // The inline CSS rules are used to resize the image
+      //
+      largeImage.src = imageURI;
+    }
+
+    // A button will call this function
+    //
+    function capturePhoto() {
+      // Take picture using device camera and retrieve image as base64-encoded string
+      navigator.camera.getPicture(onPhotoDataSuccess, onFail, { quality: 50,
+        destinationType: destinationType.DATA_URL });
+    }
+
+    // A button will call this function
+    //
+    function capturePhotoEdit() {
+      // Take picture using device camera, allow edit, and retrieve image as base64-encoded string
+      navigator.camera.getPicture(onPhotoDataSuccess, onFail, { quality: 20, allowEdit: true,
+        destinationType: destinationType.DATA_URL });
+    }
+
+    // A button will call this function
+    //
+    function getPhoto(source) {
+      // Retrieve image file location from specified source
+      navigator.camera.getPicture(onPhotoURISuccess, onFail, { quality: 50,
+        destinationType: destinationType.FILE_URI,
+        sourceType: source });
+    }
+
+    // Called if something bad happens.
+    //
+    function onFail(message) {
+      alert('Failed because: ' + message);
+    }
+//////////////////////////////////////////////////////////////////////////////////////////////
+
+function onSuccess(position) {
+        var element = document.getElementById('geolocation');
+        element.innerHTML = 'Latitude: '           + position.coords.latitude              + '<br />' +
+                            'Longitude: '          + position.coords.longitude             + '<br />' +
+                            'Altitude: '           + position.coords.altitude              + '<br />' +
+                            'Accuracy: '           + position.coords.accuracy              + '<br />' +
+                            'Altitude Accuracy: '  + position.coords.altitudeAccuracy      + '<br />' +
+                            'Heading: '            + position.coords.heading               + '<br />' +
+                            'Speed: '              + position.coords.speed                 + '<br />' +
+                            'Timestamp: '          + position.timestamp                    + '<br />';
+    }
+
+alert('Latitude: ' + position.coords.latitude + '\n');
+
+    // onError Callback receives a PositionError object
+    //
+    function onError(error) {
+        alert('code: '    + error.code    + '\n' +
+              'message: ' + error.message + '\n');
+    }
